6b746060a74b5e58145e0acb0c20a7ded7c8b380
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2024-03-22 16:21:22 +0000

    Some bug fixed

--- cookbook-frontend/src/server.cjs
+++ cookbook-frontend/src/server.cjs
@@ -30,7 +30,7 @@ app.use(cors({
 // Define your routes and other server configuration below this line
 
 // Start the server
-const PORT = process.env.PORT || 8081; // Use port 8080 as an example
+const PORT = process.env.PORT || 5173; // Use port 8080 as an example
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
